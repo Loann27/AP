bb24610754d0d0c7ed3c6898b58e513aed3a7c7d
Loann27 <loann.piquet@gmail.com>
2024-12-13 14:00:39 +0100

    Update variables.js

--- variables.js
+++ variables.js
@@ -33,6 +33,7 @@ let doc_vitale;
 let doc_mutuelle;
 let doc_livret;
 let erreur = false;
+//var xhr = new XMLHttpRequest(); A faire pour AJAX
 pages();
 
 function getISODate(){
@@ -45,17 +46,17 @@ function getISODate(){
 
 function pages() {
     if(page == 0) {
-        document.getElementById("affichage").innerHTML = "<img src='../../images/Hospitalisation.png' class='center'>" + "<div id='bloc1'>" + "<form onsubmit='hospitalisation();return false'>" + "<label for='preadmi'>Pré-admission pour:*</label>" + "<select name='preadmi' id='preadmi' required>" + "<option value=''>Choix</option>" + "<option value='Ambulatoire chirurgie'>Ambulatoire chirurgie</option>" + "<option value='Hospitalisation'>Hospitalisation (au moins une nuit)</option>" + "</select><br><br>" + "<label for='date_hospi'>Date hospitalisation:* </label>" + "<input id='date_hospi' name='date_hospi' type='date' required><br/>" + "<label for='heure'>Heure de l'intervention (7:00 - 16:30):* </label>" + "<input id='heure' name='heure' type='time' placeholder='--:--' onchange='verif_time(this.value)' required><br><br>" + "<label for='medecin'>Nom du médecin*</label><br>" + "<select name='medecin' id='medecin' required>" + "<option value=''>Choix</option>" + "<option value='Alexandrie Covillon'>Alexandrie COVILLON (Maxillo-facial)</option>" + "<option value='Françoise Marquis'>Françoise MARQUIS (Radiologue)</option>" + "<option value='Hugues Faure'>Hugues FAURE (Neurologue)</option>" + "</select><br><br>" + "<input type='submit' id='submit' name='submit' value='Suivant >'>" + "</form>" + "</div>" + "<div id='erreur'>" + "<p id='aff_erreur'>" + "</div>";
+        document.getElementById("affichage").innerHTML = "<img src='../../images/Hospitalisation.png' class='center'>" + "<div id='bloc1'>" + "<form onsubmit='hospitalisation();return false'>" + "<label for='preadmi'>Pré-admission pour:*</label>" + "<select name='preadmi' id='preadmi' required>" + "<option value=''>Choix</option>" + "<option value='Ambulatoire chirurgie'>Ambulatoire chirurgie</option>" + "<option value='Hospitalisation'>Hospitalisation (au moins une nuit)</option>" + "</select><br><br>" + "<label for='date_hospi'>Date hospitalisation:* </label>" + "<input id='date_hospi' name='date_hospi' type='date' onchange='verif()' required><br/>" + "<label for='heure'>Heure de l'intervention (7:00 - 16:30):* </label>" + "<input id='heure' name='heure' type='time' placeholder='--:--' onchange='verif_time(this.value)' required><br><br>" + "<label for='medecin'>Nom du médecin*</label><br>" + "<select name='medecin' id='medecin' required>" + "<option value=''>Choix</option>" + "<option value='Alexandrie Covillon'>Alexandrie COVILLON (Maxillo-facial)</option>" + "<option value='Françoise Marquis'>Françoise MARQUIS (Radiologue)</option>" + "<option value='Hugues Faure'>Hugues FAURE (Neurologue)</option>" + "</select><br><br>" + "<input type='submit' id='submit' name='submit' value='Suivant >'>" + "</form>" + "</div>" + "<div id='erreur'>" + "<p id='aff_erreur'>" + "</div>";
         document.getElementById('date_hospi').setAttribute('min',getISODate());
     } else if(page == 1) {
-        document.getElementById("affichage").innerHTML = "<img src='../../images/Patient.png' class='center'>" + "<div id='bloc1'>" + "<h1>Informations concernant le patient</h1>" + "<form onsubmit='patients();return false'>" + "<label for='civ'>Civ. </label>" + "<select name='civ' id='civ' required>" + "<option value=''>Choix</option>" + "<option value='Homme'>Homme</option>" + "<option value='Femme'>Femme</option>" + "</select>" + "<label for='nom_naissance'>Nom de naissance </label>" + "<input id='nom_naissance' name='nom_naissance' type='text' required>" + "<label for='nom_epouse'>Nom d'épouse </label>" + "<input id='nom_epouse' name='nom_epouse' type='text'><br><br>" + "<label for='prenom'>Prénom </label>" + "<input type='text' id='prenom' name='prenom' required>"  + "<label for='date_naissance'>Date de naissance </label>" + "<input type='date' id='date_naissance' name='date_naissance' required><br><br>" + "<label for='adresse'>Adresse </label>" + "<input type='text' id='adresse' name='adresse' required><br><br>" + "<label for='cp'>Code Postal </label>" + "<input type='tel' id='cp' name='cp' pattern='[0-9]{5}' required>" + "<label for='ville'>Ville </label>" + "<input type='text' id='ville' name='ville' required><br><br>" + "<label for='email'>Email </label>" + "<input type='mail' id='email' name='email' required>" + "<label for='telephone'>Téléphone </label>" + "<input type='tel' id='telephone' name='telephone' pattern='[0-9]{10}' required><br><br>" + "<input type='submit' id='submit' name='submit' value='Suivant >'>" + "</form>" + "<input type='submit' onclick='precedent();return false' value='précédent'>" + "</div>";
+        document.getElementById("affichage").innerHTML = "<img src='../../images/Patient.png' class='center'>" + "<div id='bloc1'>" + "<h1>Informations concernant le patient</h1>" + "<form onsubmit='patients();return false'>" + "<label for='civ'>Civ. </label>" + "<select name='civ' id='civ' required>" + "<option value=''>Choix</option>" + "<option value='Homme'>Homme</option>" + "<option value='Femme'>Femme</option>" + "</select><br/>" + "<label for='nom_naissance'>Nom de naissance </label>" + "<input id='nom_naissance' name='nom_naissance' type='text' required><br/>" + "<label for='nom_epouse'>Nom d'épouse </label>" + "<input id='nom_epouse' name='nom_epouse' type='text'><br><br>" + "<label for='prenom'>Prénom </label>" + "<input type='text' id='prenom' name='prenom' required><br/>"  + "<label for='date_naissance'>Date de naissance </label>" + "<input type='date' id='date_naissance' name='date_naissance' required><br><br>" + "<label for='adresse'>Adresse </label>" + "<input type='text' id='adresse' name='adresse' required><br><br>" + "<label for='cp'>Code Postal </label>" + "<input type='tel' id='cp' name='cp' pattern='[0-9]{5}' required><br/>" + "<label for='ville'>Ville </label>" + "<input type='text' id='ville' name='ville' required><br><br>" + "<label for='email'>Email (.com, .fr, .en, .net, .co.uk)</label>" + "<input type='mail' id='email' name='email' required><br/><br/>" + "<label for='telephone'>Téléphone </label>" + "<input type='tel' id='telephone' name='telephone' pattern='[0-9]{10}' required><br><br>" + "<input type='submit' id='submit' name='submit' value='Suivant >'>" + "</form>" + "<input type='submit' onclick='precedent();return false' value='précédent'>" + "</div>";
         document.getElementById('date_naissance').setAttribute('max',getISODate());
     } else if(page == 2) {
-        document.getElementById("affichage").innerHTML = "<img src='../../images/Patient.png' class='center'>" + "<div id='bloc1'>" + "<h1>Coordonnées personne à prévenir</h1>" + "<form onsubmit='prevenir();return false'>" + "<label for='nom_prev'>Nom </label>" + "<input type='text' name='nom_prev' id='nom_prev'>" + "<label for='pren_prev'>Prénom </label>" + "<input type='text' name='pren_prev' id='pren_prev'>" + "<label for='tel_prev'>Téléphone </label>" + "<input type='tel' name='tel_prev' id='tel_prev' pattern='[0-9]{10}'><br>" + "<label for='adr_prev'>Adresse</label>" + "<input type='text' name='adr_prev' id='adr_prev'>" + "<input type='submit' name='submit' id='submit' value='Suivant >'>" + "</form>" + "<input type='submit' onclick='precedent();return false' value='précédent'>" + "</div>";
+        document.getElementById("affichage").innerHTML = "<img src='../../images/Patient.png' class='center'>" + "<div id='bloc1'>" + "<h1>Coordonnées personne à prévenir</h1>" + "<form onsubmit='prevenir();return false'>" + "<label for='nom_prev'>Nom </label>" + "<input type='text' name='nom_prev' id='nom_prev' required><br/>" + "<label for='pren_prev'>Prénom </label>" + "<input type='text' name='pren_prev' id='pren_prev' required><br/>" + "<label for='tel_prev'>Téléphone </label>" + "<input type='tel' name='tel_prev' id='tel_prev' pattern='[0-9]{10}' required><br/>" + "<label for='adr_prev'>Adresse</label>" + "<input type='text' name='adr_prev' id='adr_prev' required><br/>" + "<input type='submit' name='submit' id='submit' value='Suivant >'>" + "</form>" + "<input type='submit' onclick='precedent();return false' value='précédent'>" + "</div>";
     } else if(page == 3) {
-        document.getElementById("affichage").innerHTML = "<img src='../../images/Patient.png' class='center'>" + "<div id='bloc1'>" + "<h1>Coordonnées personne de confiance</h1>" + "<form onsubmit='confiance();return false'>" + "<label for='nom_conf'>Nom </label>" + "<input type='text' name='nom_conf' id='nom_conf'>" + "<label for='pren_conf'>Prénom </label>" + "<input type='text' name='pren_conf' id='pren_conf'>" + "<label for='tel_conf'>Téléphone </label>" + "<input type='tel' name='tel_conf' id='tel_conf' pattern='[0-9]{10}'><br>" + "<label for='adr_conf'>Adresse</label>" + "<input type='text' name='adr_conf' id='adr_conf'>" + "<input type='submit' name='submit' id='submit' value='Suivant >'>" + "</form>" + "<input type='submit' onclick='precedent();return false' value='précédent'>" + "</div>";
+        document.getElementById("affichage").innerHTML = "<img src='../../images/Patient.png' class='center'>" + "<div id='bloc1'>" + "<h1>Coordonnées personne de confiance</h1>" + "<form onsubmit='confiance();return false'>" + "<label for='nom_conf'>Nom </label>" + "<input type='text' name='nom_conf' id='nom_conf' required><br/>" + "<label for='pren_conf'>Prénom </label>" + "<input type='text' name='pren_conf' id='pren_conf' required><br/>" + "<label for='tel_conf'>Téléphone </label>" + "<input type='tel' name='tel_conf' id='tel_conf' pattern='[0-9]{10}' required><br/>" + "<label for='adr_conf'>Adresse</label>" + "<input type='text' name='adr_conf' id='adr_conf' required><br/>" + "<input type='submit' name='submit' id='submit' value='Suivant >'>" + "</form>" + "<input type='submit' onclick='precedent();return false' value='précédent'>" + "</div>";
     } else if(page == 4) {
-        document.getElementById("affichage").innerHTML = "<img src='../../images/couvert_sociale.png' class='center'>" + "<div id='bloc1'>" + "<form onsubmit='couv_sociale();return false'>" + "<label for='orga'>Organisme de sécurité sociale / Nom de la caisse d'assurance maladie* </label>" + "<input type='text' name='orga' id='orga' placeholder='Ex: CPAM du Tarn et Garonne, CPAM du Lot, RSI, MSA...' required><br>" + "<label for='num_secu'>Numéro de sécurité sociale* </label>" + "<input type='tel' name='num_secu' id='num_secu' pattern='[0-9]{13}' required><br>" + "<label for='assure'>Le patient est-il assuré?* </label>" + "<select name='assure' id='assure' required>" + "<option value=''>Choix</option>" + "<option value='oui'>Oui</option>" + "<option value='non'>Non</option>" + "</select>" + "<label for='ald'>Le patient est-il ALD?* </label>" + "<select name='ald' id='ald' required>" + "<option value=''>Choix</option>" + "<option value='oui'>Oui</option>" + "<option value='non'>Non</option>" + "</select><br><br>" + "<label for='nom_mutu'>Nom de la mutuelle ou de l'assurance* </label>" + "<input type='text' name='nom_mutu' id='nom_mutu' required>" + "<label for='num_adherent'>Numéro adhérent* </label>" + "<input type='tel' name='num_adherent' id='num_adherent' required>" + "<label for='chambre_part'>Chambre particulière?* </label>" + "<select name='chambre_part' id='chambre_part' required>" + "<option value=''>Choix</option>" + "<option value='oui'>Oui</option>" + "<option value='non'>Non</option>" + "</select><br>" + "<input type='submit' name='submit' id='submit' value='Suivant >'>" + "</form>" + "<input type='submit' value='précédent' onclick='precedent();return false'>" + "</div>" + "<div id='erreur'>" + "<p id='aff_erreur'>" + "</div>";
+        document.getElementById("affichage").innerHTML = "<img src='../../images/couvert_sociale.png' class='center'>" + "<div id='bloc1'>" + "<form onsubmit='couv_sociale();return false'>" + "<label for='orga'>Organisme de sécurité sociale / Nom de la caisse d'assurance maladie* </label>" + "<input type='text' name='orga' id='orga' placeholder='Ex: CPAM du Tarn et Garonne, CPAM du Lot, RSI, MSA...' required><br/>" + "<label for='num_secu'>Numéro de sécurité sociale* </label>" + "<input type='tel' name='num_secu' id='num_secu' pattern='[0-9]{13}' required><br/>" + "<label for='assure'>Le patient est-il assuré?* </label>" + "<select name='assure' id='assure' required>" + "<option value=''>Choix</option>" + "<option value='oui'>Oui</option>" + "<option value='non'>Non</option>" + "</select><br/>" + "<label for='ald'>Le patient est-il ALD?* </label>" + "<select name='ald' id='ald' required>" + "<option value=''>Choix</option>" + "<option value='oui'>Oui</option>" + "<option value='non'>Non</option>" + "</select><br/>" + "<label for='nom_mutu'>Nom de la mutuelle ou de l'assurance* </label>" + "<input type='text' name='nom_mutu' id='nom_mutu' required><br/>" + "<label for='num_adherent'>Numéro adhérent* </label>" + "<input type='tel' name='num_adherent' id='num_adherent' required><br/>" + "<label for='chambre_part'>Chambre particulière?* </label>" + "<select name='chambre_part' id='chambre_part' required>" + "<option value=''>Choix</option>" + "<option value='oui'>Oui</option>" + "<option value='non'>Non</option>" + "</select><br/>" + "<input type='submit' name='submit' id='submit' value='Suivant >'>" + "</form>" + "<input type='submit' value='précédent' onclick='precedent();return false'>" + "</div>" + "<div id='erreur'>" + "<p id='aff_erreur'>" + "</div>";
     } else if(page == 5) {
         document.getElementById("affichage").innerHTML = "<img src='../../images/documents.png' class='center'>" + "<div id='bloc1'>" + "<form onsubmit='doc();return false'>" + "<label for='identity'>Carte d'identité (recto / verso):</label>" + "<input type='file' id='identity' accept='.jpg, .png, .pdf' required>" + "<br/>" + "<label for='carteVitale'>Carte vitale:</label>" + "<input type='file' id='carteVitale' accept='.jpg, .png, .pdf' required>" + "<br/>" + "<label for='mutuelle'>Carte de mutuelle:</label>" + "<input type='file' id='mutuelle' name='mutuelle' accept='.jpg, .png, .pdf' required>" + "<br/>" + "<label for='livretFamille'>Livret de famille (pour enfants mineurs):</label>" + "<input type='file' id='livretFamille' name='livretFamille' accept='.jpg, .png, .pdf'>" + "<br/>" + "<input type='submit' name='submit' id='submit' value='Envoyer'>" + "</form>" + "<input type='submit' value='précédent' onclick='precedent();return false'>" + "</div>";
     }
@@ -67,11 +68,20 @@ function hospitalisation() {
     heure = document.getElementById("heure").value;
     medecin = document.getElementById("medecin").value;
     if(erreur == true) {
-        alert("Une erreur est survenue!");
+        alert("Heure incorrecte!");
         return;
     }
     page = page + 1;
     pages();
+}
+
+function log(value) {
+    console.log(value);
+}
+
+function verif() {
+    heure = document.getElementById("heure").value;
+    verif_time(heure);
 }
 
 function verif_time(time) {
@@ -80,8 +90,26 @@ function verif_time(time) {
         message4();
         erreur = true;
     } else {
-        document.getElementById("aff_erreur").innerHTML = "";
-        erreur = false;
+        const d = new Date();
+        const yyyy = d.getFullYear();
+        let mm = d.getMonth() + 1;
+        let dd = d.getDate();
+
+        if (dd < 10) dd = '0' + dd;
+        if (mm < 10) mm = '0' + mm;
+
+        const Today = yyyy + "-" + mm + "-" + dd;
+        const heures = d.getHours();
+        const minutes = d.getMinutes();
+        const date = document.getElementById("date_hospi").value;
+        log(Today);
+        if((heures > hours || (heures == hours && minutes > mins)) && (Today == date)) {
+            message4();
+            erreur = true;
+        } else {
+            document.getElementById("aff_erreur").innerHTML = "";
+            erreur = false;
+        }
     }
 }
 
@@ -96,8 +124,29 @@ function patients() {
     ville = document.getElementById("ville").value;
     email = document.getElementById("email").value;
     tel = document.getElementById("telephone").value;
+    verif_patients();
+    if(erreur == true) {
+        alert("Format d'adresse mail incorrect!");
+        return;
+    }
     page = page + 1;
     pages();
+}
+
+function verif_patients() {
+    if(email.includes("@") && (email.endsWith(".com") || email.endsWith(".fr") || email.endsWith(".en") || email.endsWith(".net") || email.endsWith(".co.uk"))) {
+        if(email.endsWith("@.com") || email.endsWith("@.fr") || email.endsWith("@.en") || email.endsWith("@.net") || email.endsWith("@.co.uk")) {
+            erreur = true;
+        } else {
+            if(email.startsWith("@")) {
+                erreur = true;
+            } else {
+                erreur = false;
+            }
+        }
+    } else {
+        erreur = true;
+    }
 }
 
 function prevenir() {
@@ -105,8 +154,21 @@ function prevenir() {
     pren_prev = document.getElementById("pren_prev").value;
     tel_prev = document.getElementById("tel_prev").value;
     adr_prev = document.getElementById("adr_prev").value;
+    verif_prevenir();
+    if(erreur == true) {
+        alert("Le téléphone ne peut pas être celui du patient!");
+        return;
+    }
     page = page + 1;
     pages();
+}
+
+function verif_prevenir() {
+    if(tel_prev == tel) {
+        erreur = true;
+    } else {
+        erreur = false;
+    }
 }
 
 function confiance() {
@@ -114,8 +176,21 @@ function confiance() {
     pren_conf = document.getElementById("pren_conf").value;
     tel_conf = document.getElementById("tel_conf").value;
     adr_conf = document.getElementById("adr_conf").value;
+    verif_confiance();
+    if(erreur == true) {
+        alert("Le téléphone ne peut pas être celui du patient!");
+        return;
+    }
     page = page + 1;
     pages();
+}
+
+function verif_confiance() {
+    if(tel_conf == tel) {
+        erreur = true;
+    } else {
+        erreur = false;
+    }
 }
 
 function couv_sociale() {
@@ -129,7 +204,7 @@ function couv_sociale() {
     chambre_part = document.getElementById("chambre_part").value;
     verif_num_secu();
     if(erreur == true) {
-        alert("Une erreur est survenue!");
+        alert("Numéro de sécurité sociale incorrect!");
         return;
     }
     page = page + 1;
