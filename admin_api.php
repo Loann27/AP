72e05c2bf8839b563f54bd9a80c4f118ce3c1186
Loann27 <loann.piquet@gmail.com>
2025-04-30 11:53:28 +0200

    Update admin_api.php

--- admin_api.php
+++ admin_api.php
@@ -1,8 +1,4 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 session_start();
 
 // Vérification de la session admin
